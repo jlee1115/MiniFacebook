db9a73d593f145cad98ff6735b7372a9e0b5da2e
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-11-26 12:43:39 -0500

    backend setup

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const path = require("path");
-let app = express();
+const routes = require("./routes/routes");
+const app = express();
+
+app.use(express.bodyParser());
+app.use(express.logger("default"));
+
+function hashPassword(password) {
+  let hash = crypto.createHash("sha256");
+  hash.update(password);
+  return hash.digest("hex");
+}
 
 app.get("/", function(req, res) {
   res.send("hellp");
