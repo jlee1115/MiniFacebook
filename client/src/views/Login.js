e03c32c539fa661cc650dd70457337b02df8b052
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-11-26 14:50:22 -0500

    form data

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -4,25 +4,42 @@ import axios from "axios";
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   handleSubmit(e) {
     e.preventDefault();
     console.log("submitted");
+    //this is correct
+    console.log(this.state);
+  }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
   render() {
     return (
       <div class="container">
         <h3>Login in PennBook</h3>
         <form onSubmit={this.handleSubmit}>
-          <input class="form-control" type="text" name="" id="" placeholder="Username" />
+          <input
+            class="form-control"
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+            onChange={this.handleChange}
+          />
           <input
             class="form-control"
             type="password"
-            name=""
-            id=""
+            name="password"
+            id="password"
             placeholder="Password"
+            onChange={this.handleChange}
           />
           <button class="btn btn-primary" type="submit" value="Login">
             Log in
