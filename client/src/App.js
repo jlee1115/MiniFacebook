e499f9e43a565d6cac3754f09186aca4f0829df7
Maria Tu <mariatu@huntsman-ve703-02823.apn.wlan.private.upenn.edu>
2019-12-18 12:17:28 -0500

    integration with chat

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,8 @@ import { BrowserRouter as Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
 import ProfilePage from "./views/ProfilePage";
 import Feed from "./views/Feed";
-<<<<<<< HEAD
 import Chat from "./views/ChatPage";
-=======
 import FriendVis from "./views/FriendVis";
->>>>>>> edc2ac3cc9cca0c28921bfb9301846b71c186632
 
 function App() {
   return (
@@ -18,11 +15,8 @@ function App() {
         <Route path="/profile" exact component={ProfilePage} />
         <Route path="/profile/:userID" exact component={ProfilePage} />
         <Route path="/feed" exact component={Feed} />
-<<<<<<< HEAD
         <Route path="/chat" exact component={Chat} />
-=======
         <Route path="/friendVis" exact component={FriendVis} />
->>>>>>> edc2ac3cc9cca0c28921bfb9301846b71c186632
       </Switch>
     </BrowserRouter>
   );
