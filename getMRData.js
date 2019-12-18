12f0c2217697da65c8a158c09df89d40b8d30469
Julie Lee <julielee1115@gmail.com>
2019-12-18 11:40:01 -0500

    change

--- getMRData.js
+++ getMRData.js
@@ -5,30 +5,17 @@ const fs = require('fs')
 users.init(function(err, data){})
 friends.init(function(errF, dataF){})
 
+let output  = '';
+
     users.scanKeys(function(err, data){
         if(err){
             console.log("Error", err);
         }else if (!data){
-            console.log("No data in table");
+            console.log("No data in table"); 
         } else {
-            let output  = '';
             for(let i = 0; i < data.length; i++){
                 output += data[i].key; 
                 output += ':'; 
-                friends.get(data[i].key, function(err2, data2){
-                    if(err2){
-                        console.log("error retrieving friends", err2);
-                    } else if (!data2){
-                        console.log("no friend data");
-                    } else {
-                        console.log("data2lengthis" + data2.length);
-                        for (let k = 0; k < data2.length; k++){
-                            
-                            output += data2[i].value;
-                            output += ",";
-                        }
-                    }
-                });
                 let values = JSON.parse(data[i].value);
                 let interests = values.interests;
                 for(let j = 0; j < interests.length; j++){
@@ -37,6 +24,28 @@ friends.init(function(errF, dataF){})
                 }
                 let affiliation = values.affiliation;
                 output += affiliation; 
+                output += '\n';
+    
+            }
+    
+            fs.writeFile('recsInput.txt', output, (err) => {
+                if (err) throw err;
+                console.log('Data successfully retrieved from database!');
+            });
+        }
+    })
+
+    friends.scanKeys(function(err2, data2){
+        if(err2){
+            console.log("Error", err2);
+        }else if (!data2){
+            console.log("No data in table"); 
+        } else {
+            for(let i = 0; i < data2.length; i++){
+                output += data2[i].key; 
+                output += ':'; 
+                
+                output += data2[i].value;
                 output += '\n';
     
             }
